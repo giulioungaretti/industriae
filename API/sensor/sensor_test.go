ad17209df2682a0f9afdda080e26c8d0244ad0ec
giulio ungaretti <giulio.ungaretti@lego.com>
2023-08-02 12:20:05 +0000

    ensure and test that sensor respects its limits

--- API/sensor/sensor_test.go
+++ API/sensor/sensor_test.go
@@ -14,7 +14,8 @@ func TestSensor_Run(t *testing.T) {
 	// as it will take one tick
 	scanTime := 1
 	setpoint := 10
-	s := sensor.Create("test", scanTime)
+	setPointLimit := 10
+	s := sensor.Create("test", scanTime, setPointLimit)
 	// set point
 	s.ControlCh <- setpoint
 	//
@@ -27,6 +28,33 @@ func TestSensor_Run(t *testing.T) {
 	expected = <-s.ReadCh
 	value := expected.Value()
 	if value != setpoint {
+		t.Errorf("unexpected value: %v", value)
+	}
+}
+
+func TestSensor_Error(t *testing.T) {
+	// create mock control and read channels
+	// set the scan rate to 1 irco seconds
+	// this is a cheat to predict the value of the sensor
+	// as it will take one tick
+	scanTime := 1
+	setpoint := 10
+	setPointLimit := 0
+	s := sensor.Create("test", scanTime, setPointLimit)
+	// set setpoint to much higher value
+	s.ControlCh <- setpoint
+	expected := <-s.ReadCh
+	if expected.SetPoint() == setpoint {
+		t.Errorf("unexpected setpoint: %v should have not ben set over the limit", expected.SetPoint())
+	}
+	if expected.Error() == nil {
+		t.Errorf("Expected error")
+	}
+	// wait for the sensor to read the value
+	time.Sleep(100 * time.Millisecond)
+	expected = <-s.ReadCh
+	value := expected.Value()
+	if value == setpoint {
 		t.Errorf("unexpected value: %v", value)
 	}
 }
